6cf6fa3be454b19ce6755967770ebb421faaac2b
AETHEL <149665445+CH3NG4M1NG@users.noreply.github.com>
2026-04-01 13:54:28 +0700

    Delete universal_converter.py

--- universal_converter.py
+++
@@ -1,7 +0,0 @@
-from resonait.tools.alignment import LLMAlignmentTool, AlignmentConfig, FrequencyAlignmentLayer
-from resonait.tools.unified_trainer import UnifiedTrainer, UnifiedTrainerConfig
-
-__all__ = [
-    "LLMAlignmentTool", "AlignmentConfig", "FrequencyAlignmentLayer",
-    "UnifiedTrainer", "UnifiedTrainerConfig",
-]
