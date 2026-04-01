2a96e793ebc7cf6a4da5cc17ace81270ee75b2b6
AETHEL <149665445+CH3NG4M1NG@users.noreply.github.com>
2026-04-01 13:51:25 +0700

    Delete __init__ (2).py

--- __init__ (2).py
+++
@@ -1,8 +0,0 @@
-from resonait.core.brain import ResonAItBrain
-from resonait.core.frequency_space import FrequencyTensor, UniversalFrequencySpace, Modality
-from resonait.core.unified_model import ResonAItUnified, UnifiedInput, UnifiedOutput, TaskType
-
-__all__ = [
-    "ResonAItBrain", "FrequencyTensor", "UniversalFrequencySpace",
-    "Modality", "ResonAItUnified", "UnifiedInput", "UnifiedOutput", "TaskType"
-]
