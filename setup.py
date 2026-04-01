469242af0bcdb1955c563422d5e4cdbead759d65
AETHEL <149665445+CH3NG4M1NG@users.noreply.github.com>
2026-04-01 13:53:42 +0700

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""
-resonait/converters/audio_converter.py
-========================================
-Alias/re-export AudioConverter dari image_converter.py
-(AudioConverter didefinisikan di image_converter.py untuk kemudahan)
-"""
-from resonait.converters.image_converter import AudioConverter
-
-__all__ = ["AudioConverter"]
